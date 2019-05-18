64c4201bc7ec8aa117e98a5cc8b9f54130e6a787
Danielle Morton <danielle@ammosquared.com>
2019-05-17 19:36:41 -0600

    some prettification

--- app/models/Song.js
+++ app/models/Song.js
@@ -12,16 +12,19 @@ export default class Song {
     get Template() {
         //html goes here
         return `
-<ul>
-    <li>
-        <img src="${this.albumArt}" alt="">
-        <h2>${this.artist}</h2>
-        <h5>${this.price}</h5>
-        <audio controls>
-            <source src="${this.preview}" type="audio/mp3">
-        </audio>
-    </li>
-</ul>
+<div class="col" >
+        <ul>
+            <li>
+            <img src="${this.albumArt}" alt="">
+            <h2>${this.artist}</h2>
+            <h3>${this.title}</h3>
+            <h5>$${this.price}</h5>
+            <audio controls>
+                <source src="${this.preview}" type="audio/mp3">
+            </audio>
+        </li>
+    </ul>
+</div>
 `
     }
 }
